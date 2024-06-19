c00c5acbd7e727f70363c73c5dead462f8b9f65f
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-06-19 20:05:49 +0700

    feat(api): add seeding before starting

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -47,7 +47,9 @@ var services = scope.ServiceProvider;
 
 try {
     var context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+
 }catch(Exception ex) {
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during migration");
