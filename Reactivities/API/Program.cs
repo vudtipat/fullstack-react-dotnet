360ca643a8b9e2be4aaa81445a4be421fba3f98b
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-06-19 19:46:20 +0700

    feat(api): add dbcontext and migration db with ef

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
--- Reactivities/Domain/Activity.cs
+++ Reactivities/Domain/Activity.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class Activities
+    public class Activity
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
