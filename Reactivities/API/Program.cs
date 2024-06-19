70da136e79d9afde9ad7e5968c482d2cd5197238
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-06-19 21:01:43 +0700

    feat(api): add cors on api

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddDbContext<DataContext>(opt => {
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy",policy => {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +31,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseSwaggerUI();
 app.MapControllers();
+app.UseCors("CorsPolicy");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
